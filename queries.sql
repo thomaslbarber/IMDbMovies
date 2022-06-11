5d55c21d191e1d86d6f033c14004e01ab8adaa84
thomaslbarber <thomaslbarber1@gmail.com>
2022-06-11 03:34:56 +0100

    Added a query that counts the frequency of each number rating.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 USE imdb_movies;
 
+# Queries
+# The frequency of each rating
+CREATE VIEW RatingFrequency AS
+SELECT averageRating, COUNT(averageRating) AS numberOfOccurences, COUNT(averageRating) / (SELECT COUNT(averageRating) FROM Ratings) as percentage
+FROM Ratings
+GROUP BY averageRating 
+ORDER BY averageRating DESC;
 
-
+SELECT * FROM RatingFrequency;
